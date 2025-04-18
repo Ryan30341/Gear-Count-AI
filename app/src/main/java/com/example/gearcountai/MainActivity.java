ff3e146c71b6969f796853ce853462a9f2d2d6ac
Ryan30341 <R.Cunningham@RECRoboticsDesigns.com>
2025-04-18 10:38:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/gearcountai/MainActivity.java
@@ -0,0 +1,31 @@
+// MainActivity.java
+package com.example.gearcountai;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.*;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    // (Omitted for brevity: see canvas)
+    ...
+}
